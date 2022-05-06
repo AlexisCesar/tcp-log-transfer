531089dd5aabc939a6aa58388ddb7b79896fa55a
Alexis Cesar <alexis.almeida@programmers.com.br>
2022-05-06 11:16:39 -0300

    Added LogRegister model

---
+++ client/AccessLogRegister.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+
+namespace client
+{
+    internal class AccessLogRegister
+    {
+        public AccessLogRegister()
+        {
+
+        }
+
+        public AccessLogRegister(string sourceIPAddress, string destinationIPAddress, DateTime brazilianTime, string url, HttpStatusCode statusCode, int requestBytes)
+        {
+            SourceIPAddress = sourceIPAddress;
+            DestinationIPAddress = destinationIPAddress;
+            BrazilianTime = brazilianTime;
+            Url = url;
+            StatusCode = statusCode;
+            RequestBytes = requestBytes;
+        }
+
+        public string? SourceIPAddress { get; set; }
+        public string? DestinationIPAddress { get; set; }
+        public DateTime BrazilianTime { get; set; }
+        public string? Url { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public int RequestBytes { get; set; }
+    }
+}
