a6d95b9d86787116fa3dec4c3fb40b9d2813b5ee
Alexis Cesar <alexis.almeida@programmers.com.br>
2022-05-26 07:57:51 -0300

    Added stopwatch and changed to send 200k

--- client/Program.cs
+++ client/Program.cs
@@ -1,48 +1,49 @@
-﻿using System.Net.Sockets;
+﻿using System.Diagnostics;
+using System.Net.Sockets;
 using System.Text.Json;
 using client;
 
 static void Connect(String server, IEnumerable<string> messages)
 {
-  try
-  {
-    // Create a TcpClient.
-    Int32 port = 13000;
-    TcpClient client = new TcpClient(server, port);
+    try
+    {
+        // Create a TcpClient.
+        Int32 port = 13000;
+        TcpClient client = new TcpClient(server, port);
 
-    // Get a client stream for reading and writing.
-    NetworkStream stream = client.GetStream();
-    StreamWriter sw = new StreamWriter(stream);
+        // Get a client stream for reading and writing.
+        NetworkStream stream = client.GetStream();
+        StreamWriter sw = new StreamWriter(stream);
 
-    foreach (string individualMessage in messages) {
-        // Send the message to the connected TcpServer.
-        sw.WriteLine(individualMessage);
+        foreach (string individualMessage in messages)
+        {
+            // Send the message to the connected TcpServer.
+            sw.WriteLine(individualMessage);
+        }
+
+
+        // Close everything.
+        sw.Close();
+        stream.Close();
+        client.Close();
     }
-    
-
-    // Close everything.
-    sw.Close();
-    stream.Close();
-    client.Close();
-  }
-  catch (ArgumentNullException e)
-  {
-    Console.WriteLine("ArgumentNullException: {0}", e);
-  }
-  catch (SocketException e)
-  {
-    Console.WriteLine("SocketException: {0}", e);
-  }
+    catch (ArgumentNullException e)
+    {
+        Console.WriteLine("ArgumentNullException: {0}", e);
+    }
+    catch (SocketException e)
+    {
+        Console.WriteLine("SocketException: {0}", e);
+    }
 }
 
 // ----------------------------------
 
+Stopwatch stopwatch = new();
+stopwatch.Start();
+
 var messages = new List<string>();
 var limitCount = 0;
-
-// Elapsed time vars
-TimeSpan elapsedTime;
-DateTime startTime, endTime;
 
 long readLinesCount = 0;
 
@@ -55,35 +56,33 @@ using (StreamReader sr = new StreamReader(bs))
 
     while ((line = sr.ReadLine()) != null)
     {
-        if(String.IsNullOrEmpty(line)) continue;
+        if (String.IsNullOrEmpty(line)) continue;
         // For each line of the log file:
         var myObj = logProcessor.processLogLineAndReturnIt(line);
 
 
-        if (myObj == null) {
+        if (myObj == null)
+        {
             continue;
         }
 
         var jsonObj = JsonSerializer.Serialize(myObj);
 
-        if (jsonObj == null) {
+        if (jsonObj == null)
+        {
             continue;
         }
 
         messages.Add(jsonObj);
 
         limitCount++;
-        if(limitCount >= 100000) {
+        if (limitCount >= 200000)
+        {
             //SEND Messages
-            startTime = DateTime.Now;
-
             Console.WriteLine("Sending...");
             Connect("127.0.0.1", messages);
 
-            endTime = DateTime.Now;
-            elapsedTime = endTime.Subtract(startTime);
-
-            Console.WriteLine($"Sent {messages.Count} messages. In {elapsedTime} milliseconds.");
+            Console.WriteLine($"Sent {messages.Count} messages.");
             messages.Clear();
             GC.Collect();
             //SEND Messages
@@ -97,13 +96,10 @@ using (StreamReader sr = new StreamReader(bs))
 
 Console.WriteLine($"Processed {readLinesCount} lines.");
 
-startTime = DateTime.Now;
-
 Console.WriteLine("Sending...");
 Connect("127.0.0.1", messages);
 
-endTime = DateTime.Now;
+stopwatch.Stop();
+TimeSpan elapsedTime = stopwatch.Elapsed;
 
-elapsedTime = endTime.Subtract(startTime);
-
-Console.WriteLine($"Sent {messages.Count} messages. In {elapsedTime} milliseconds.");
+Console.WriteLine($"Elapsed time: {elapsedTime.Hours}:{elapsedTime.Minutes}:{elapsedTime.Seconds}.{elapsedTime.Milliseconds}.");
--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text.Json;
@@ -7,119 +8,128 @@ using server;
 
 class MyTcpListener
 {
-  public static void Main()
-  {
-    TcpListener? server=null;
-    try
+    public static void Main()
     {
-      // Set the TcpListener on port 13000.
-      Int32 port = 13000;
-      IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-
-      // TcpListener server = new TcpListener(port);
-      server = new TcpListener(localAddr, port);
-
-      // Start listening for client requests.
-      server.Start();
-
-      // Buffer for reading data
-      Byte[] bytes = new Byte[256];
-      String? data = null;
-
-      // Enter the listening loop.
-      while(true)
-      {
-        Console.Write("Waiting for a connection... ");
-
-        // Perform a blocking call to accept requests.
-        TcpClient client = server.AcceptTcpClient();
-        Console.WriteLine("Connected!");
-
-        data = null;
-
-        // Get a stream object for reading and writing
-        NetworkStream stream = client.GetStream();
-        StreamReader sr = new StreamReader(stream);
-
-        //DATABASE TABLE
-        DataTable dbTable = new DataTable();
-        dbTable.Columns.Add(new DataColumn("Id", typeof(Int32)));
-        dbTable.Columns.Add(new DataColumn("SourceIPAddress", typeof(string)));
-        dbTable.Columns.Add(new DataColumn("DestinationIPAddress", typeof(string)));
-        dbTable.Columns.Add(new DataColumn("BrazilianTime", typeof(DateTime)));
-        dbTable.Columns.Add(new DataColumn("Url", typeof(string)));
-        dbTable.Columns.Add(new DataColumn("StatusCode", typeof(Int32)));
-        dbTable.Columns.Add(new DataColumn("RequestBytes", typeof(Int32)));
-        //DATABASE TABLE
-
-        int messageCount = 0;
-
-        // Loop to receive all the data sent by the client.
-        while(sr.Peek() >= 0)
+        TcpListener? server = null;
+        try
         {
-          data = sr.ReadLine();
-          messageCount++;
-        
-          var register = JsonSerializer.Deserialize<AccessLogRegister>(data);
+            Stopwatch stopwatch = new();
+            // Set the TcpListener on port 13000.
+            Int32 port = 13000;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
 
-          if(register != null) {
-              DataRow dbRow = dbTable.NewRow();
-                dbRow["SourceIPAddress"] = register.SourceIPAddress == null ? DBNull.Value : register.SourceIPAddress;
-                dbRow["DestinationIPAddress"] = register.DestinationIPAddress == null ? DBNull.Value : register.DestinationIPAddress;
-                dbRow["BrazilianTime"] = register.BrazilianTime == null ? DBNull.Value : register.BrazilianTime;
-                dbRow["Url"] = register.Url == null ? DBNull.Value : register.Url;
-                dbRow["StatusCode"] = register.StatusCode == null ? DBNull.Value : register.StatusCode;
-                dbRow["RequestBytes"] = register.RequestBytes == null ? DBNull.Value : register.RequestBytes;
-            dbTable.Rows.Add(dbRow);
-          }
-        }
+            // TcpListener server = new TcpListener(port);
+            server = new TcpListener(localAddr, port);
 
-        // Shutdown and end connection
-        client.Close();
-        Console.WriteLine($"Received {messageCount} messages.");
-        messageCount = 0;
+            // Start listening for client requests.
+            server.Start();
 
-        // STORE DATA INTO DATABASE
-          using(SqlConnection connection = new SqlConnection("Server=localhost,1433;Database=MyDatabase;User Id=sa;Password=Database!2022;")){
+            // Buffer for reading data
+            Byte[] bytes = new Byte[256];
+            String? data = null;
 
-            using(SqlBulkCopy bulkCopy = new SqlBulkCopy(connection)) {
+            // Enter the listening loop.
+            while (true)
+            {
+                Console.Write("Waiting for a connection... ");
 
-                bulkCopy.DestinationTableName = "accessLog";
+                // Perform a blocking call to accept requests.
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+                stopwatch.Start();
 
-                bulkCopy.ColumnMappings.Add("SourceIPAddress", "SourceIPAddress");
-                bulkCopy.ColumnMappings.Add("DestinationIPAddress", "DestinationIPAddress");
-                bulkCopy.ColumnMappings.Add("BrazilianTime", "BrazilianTime");
-                bulkCopy.ColumnMappings.Add("Url", "Url");
-                bulkCopy.ColumnMappings.Add("StatusCode", "StatusCode");
-                bulkCopy.ColumnMappings.Add("RequestBytes", "RequestBytes");
+                data = null;
 
-                connection.Open();
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+                StreamReader sr = new StreamReader(stream);
 
-                bulkCopy.WriteToServer(dbTable);
+                //DATABASE TABLE
+                DataTable dbTable = new DataTable();
+                dbTable.Columns.Add(new DataColumn("Id", typeof(Int32)));
+                dbTable.Columns.Add(new DataColumn("SourceIPAddress", typeof(string)));
+                dbTable.Columns.Add(new DataColumn("DestinationIPAddress", typeof(string)));
+                dbTable.Columns.Add(new DataColumn("BrazilianTime", typeof(DateTime)));
+                dbTable.Columns.Add(new DataColumn("Url", typeof(string)));
+                dbTable.Columns.Add(new DataColumn("StatusCode", typeof(Int32)));
+                dbTable.Columns.Add(new DataColumn("RequestBytes", typeof(Int32)));
+                //DATABASE TABLE
 
-                connection.Close();
+                int messageCount = 0;
 
-                Console.WriteLine($"Stored received messages in the database!");
+                // Loop to receive all the data sent by the client.
+                while (sr.Peek() >= 0)
+                {
+                    data = sr.ReadLine();
+                    messageCount++;
+
+                    var register = JsonSerializer.Deserialize<AccessLogRegister>(data);
+
+                    if (register != null)
+                    {
+                        DataRow dbRow = dbTable.NewRow();
+                        dbRow["SourceIPAddress"] = register.SourceIPAddress == null ? DBNull.Value : register.SourceIPAddress;
+                        dbRow["DestinationIPAddress"] = register.DestinationIPAddress == null ? DBNull.Value : register.DestinationIPAddress;
+                        dbRow["BrazilianTime"] = register.BrazilianTime == null ? DBNull.Value : register.BrazilianTime;
+                        dbRow["Url"] = register.Url == null ? DBNull.Value : register.Url;
+                        dbRow["StatusCode"] = register.StatusCode == null ? DBNull.Value : register.StatusCode;
+                        dbRow["RequestBytes"] = register.RequestBytes == null ? DBNull.Value : register.RequestBytes;
+                        dbTable.Rows.Add(dbRow);
+                    }
+                }
+
+                // Shutdown and end connection
+                client.Close();
+                Console.WriteLine($"Received {messageCount} messages.");
+                messageCount = 0;
+
+                // STORE DATA INTO DATABASE
+                using (SqlConnection connection = new SqlConnection("Server=localhost,1433;Database=MyDatabase;User Id=sa;Password=Database!2022;"))
+                {
+
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection))
+                    {
+
+                        bulkCopy.DestinationTableName = "accessLog";
+
+                        bulkCopy.ColumnMappings.Add("SourceIPAddress", "SourceIPAddress");
+                        bulkCopy.ColumnMappings.Add("DestinationIPAddress", "DestinationIPAddress");
+                        bulkCopy.ColumnMappings.Add("BrazilianTime", "BrazilianTime");
+                        bulkCopy.ColumnMappings.Add("Url", "Url");
+                        bulkCopy.ColumnMappings.Add("StatusCode", "StatusCode");
+                        bulkCopy.ColumnMappings.Add("RequestBytes", "RequestBytes");
+
+                        connection.Open();
+
+                        bulkCopy.WriteToServer(dbTable);
+
+                        connection.Close();
+
+                        Console.WriteLine($"Stored received messages in the database!");
+
+                    }
+
+                }
+
+                stopwatch.Stop();
+                TimeSpan elapsedTime = stopwatch.Elapsed;
+
+                Console.WriteLine($"Elapsed time: {elapsedTime.Hours}:{elapsedTime.Minutes}:{elapsedTime.Seconds}.{elapsedTime.Milliseconds}.");
 
             }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+        finally
+        {
+            // Stop listening for new clients.
+            server.Stop();
+        }
 
-          }
-          // STORE DATA INTO DATABASE
-
-      }
+        Console.WriteLine("\nHit enter to continue...");
+        Console.Read();
     }
-    catch(SocketException e)
-    {
-      Console.WriteLine("SocketException: {0}", e);
-    }
-    finally
-    {
-       // Stop listening for new clients.
-       server.Stop();
-    }
-
-    Console.WriteLine("\nHit enter to continue...");
-    Console.Read();
-  }
 }
 
